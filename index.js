5e0fd19085cb1f79643b3c674d0f6f8f30d09c03
Chau Tran <cctran96@gmail.com>
2021-03-22 18:24:51 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    var array = []
+    for (var i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return array
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n)
+        n--
+    }
+}
